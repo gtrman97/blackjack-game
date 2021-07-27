4a8d149a1843781e07d37ea0e378d8b97f58786a
gtrman97 <andrew.killian@g.austincc.edu>
2021-07-27 13:28:15 -0500

    added js function

--- script.js
+++ script.js
@@ -5,23 +5,28 @@ var message = "";
 var min = 2; 
 var max = 12; 
 
-var firstCard = Math.floor(Math.random() * (max - min) + min);  
-var secondCard = Math.floor(Math.random() * (max - min) + min);
+function startGame() {
 
-var sum = firstCard + secondCard; 
+    let firstCard = Math.floor(Math.random() * (max - min) + min);  
+    let secondCard = Math.floor(Math.random() * (max - min) + min);
+    
+    let sum = firstCard + secondCard; 
+    
+    console.log(sum);
+    
+    if (sum < 21) {
+        message = "Would you like a new card?"; 
+    } 
+    else if (sum === 21) {
+        message = "You've got blackjack!";
+        hasBlackJack = true; 
+    }
+    else {
+        message = "You lose!";
+        isAlive = false; 
+    }
+    
+    console.log(message);
 
-console.log(sum);
 
-if (sum < 21) {
-    message = "Would you like a new card?"; 
-} 
-else if (sum === 21) {
-    message = "You've got blackjack!";
-    hasBlackJack = true; 
 }
-else {
-    message = "You lose!";
-    isAlive = false; 
-}
-
-console.log(message);
