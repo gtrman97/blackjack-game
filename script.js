8bbdac511cf09287cb05fb02847efac49e18fc2f
gtrman97 <andrew.killian@g.austincc.edu>
2021-08-03 23:02:03 -0500

    added deck array

--- script.js
+++ script.js
@@ -12,6 +12,42 @@ let max = 11;
 
 let deck = [
 { 
+    display: "2",
+    value: 2
+},  
+{
+    display: "3",
+    value: 3
+},
+{
+    display: "4",
+    value: 4
+},
+{ 
+    display: "5",
+    value: 5
+},  
+{
+    display: "6",
+    value: 6
+},
+{
+    display: "7",
+    value: 7
+},
+{ 
+    display: "8",
+    value: 8
+},  
+{
+    display: "9",
+    value: 9
+},
+{
+    display: "10",
+    value: 10
+},
+{ 
     display: "J",
     value: 10
 },  
